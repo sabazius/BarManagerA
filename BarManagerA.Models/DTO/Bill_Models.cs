ca6d94f6582b826b57974d7687d696e92d4551bd
4ervenkov749s <dimitar_87@abv.bg>
2021-11-15 18:19:37 +0200

    15.11.2021  18:19h

---
+++ BarManagerA.Models/DTO/Bill_Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarManagerA.Models.DTO
+{
+    class Bill_Models
+    {
+        public int d_ID { get; set; }
+        //number
+
+        public float d_Amount { get; set; }
+        //mount
+
+        public bool d_BillStatus { get; set; }
+        // BillStatus enum
+
+        public string d_PaymentType { get; set; }
+        // PaymentType enum
+
+        public int d_Created { get; set; }
+        //date and time
+
+        public int d_Finished { get; set; }
+        //date and time
+
+    }
+}
