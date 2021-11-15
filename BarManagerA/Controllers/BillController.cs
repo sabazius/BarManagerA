d85cbb9222f0163af6dc1979eeac2ef4c107dbd9
4ervenkov749s <dimitar_87@abv.bg>
2021-11-15 18:36:09 +0200

    18:36

---
+++ BarManagerA/Controllers/BillController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace BarManagerA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BillController : ControllerBase
+    {
+        private readonly ILogger<TagController> _logger;
+        public BillController(ILogger<BillController> logger)
+        {
+              _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Bill_Models> Get()
+        {
+            return new List<Bill_Models>()
+            {
+                {
+                    new Bill_Models()
+                    {
+                        Id = 1,
+                        Name = "TestName"
+                    }
+                },
+                {
+                    new Bill_Models()
+                    {
+                        Id = 2,
+                        Name = "TestName2"
+                    }
+                }
+            };
+        }
+    }
+}
