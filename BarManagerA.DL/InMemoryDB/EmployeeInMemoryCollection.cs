fbc0c5a6ba9c42a8a58492b638a4b5bc5a1e1661
SimeonShumanov <sshumanov97@gmail.com>
2021-11-22 18:53:33 +0200

    Employee Model

---
+++ BarManagerA.DL/InMemoryDB/EmployeeInMemoryCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarManagerA.DL.InMemoryDB
+{
+    class Class1
+    {
+    }
+}
---
+++ BarManagerA.DL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarManagerA.DL.Interfaces
+{
+    class Class1
+    {
+    }
+}
