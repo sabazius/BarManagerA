cb99d5b14a78bcdc9c759423b6cddcd200869a1d
b.serafimov <b.b.serafimov@gmail.com>
2021-11-22 17:49:53 +0200

    Create DL and Tag TagRepository

---
+++ BarManagerA.DL/InMemoryDB/TagInMemoryCollection.cs
@@ -0,0 +1,27 @@
+﻿using BarManagerA.Models.DTO;
+using System.Collections.Generic;
+
+namespace BarManagerA.DL.InMemoryDB
+{
+    public static class TagInMemoryCollection
+    {
+        public static List<Tag> TagDb = new List<Tag>()
+        {
+            new Tag()
+            {
+                Id = 1,
+                Name = "TestNameA"
+            },
+            new Tag()
+            {
+                Id = 2,
+                Name = "TestNameB"
+            },
+            new Tag()
+            {
+                Id = 3,
+                Name = "TestNameC"
+            }
+        };
+    }
+}
--- BarManagerA/Controllers/TagController.cs
+++ BarManagerA/Controllers/TagController.cs
@@ -1,7 +1,7 @@
-﻿using BarManagerA.Models.DTO;
+﻿using BarManagerA.DL.Interfaces;
+using BarManagerA.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 
 namespace BarManagerA.Controllers
 {
@@ -10,31 +10,73 @@ namespace BarManagerA.Controllers
     public class TagController : ControllerBase
     {
         private readonly ILogger<TagController> _logger;
-        public TagController(ILogger<TagController> logger)
+        private readonly ITagRepository _tagRepository;
+
+        public TagController(ILogger<TagController> logger, ITagRepository tagRepository)
         {
             _logger = logger;
+            _tagRepository = tagRepository;
         }
 
-        [HttpGet]
-        public IEnumerable<Tag> Get()
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
         {
-            return new List<Tag>()
-            {
-                {
-                    new Tag()
-                    {
-                        Id = 1,
-                        Name = "TestName"
-                    }
-                },
-                {
-                    new Tag()
-                    {
-                        Id = 2,
-                        Name = "TestName2"
-                    }
-                }
-            };
+            var result = _tagRepository.GetAll();
+
+            if (result != null) return Ok(result);
+
+            return NoContent();
         }
+
+        [HttpGet("getById")]
+        public IActionResult Get(int id)
+        {
+            var result =  _tagRepository.GetById(id);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] Tag tag)
+        {
+            if (tag == null) return BadRequest();
+
+            var result = _tagRepository.Create(tag);
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest(id);
+
+            var result = _tagRepository.Delete(id);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update([FromBody] Tag tag)
+        {
+            if (tag == null) return BadRequest();
+
+            var searchTag = _tagRepository.GetById(tag.Id);
+
+            if (searchTag == null) return NotFound(tag);
+
+            var result = _tagRepository.Update(tag);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+
+
     }
 }
