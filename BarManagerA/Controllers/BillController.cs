0ea84380868b57fccf70c6b57b7c7ff2cca34916
4ervenkov749s <dimitar_87@abv.bg>
2021-11-29 17:37:13 +0200

    bill Service Chervenkov

--- BarManagerA/Controllers/BillController.cs
+++ BarManagerA/Controllers/BillController.cs
@@ -1,52 +1,82 @@
-﻿using BarManagerA.Models.DTO;
-using BarManagerA.Models.Enums;
+﻿using BarManagerA.BL.Interfaces;
+using BarManagerA.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 
-namespace BarManagerA.Controllers
+namespace BarManagerA.Host.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class BillController : ControllerBase
     {
         private readonly ILogger<BillController> _logger;
-        public BillController(ILogger<BillController> logger)
+        private readonly IBillService _billService;
+
+        public BillController(ILogger<BillController> logger, IBillService billService)
         {
             _logger = logger;
+            _billService = billService;
         }
 
-        [HttpGet]
-        public IEnumerable<Bill> Get()
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
         {
-            return new List<Bill>()
-            {
-                {
-                    new Bill()
-                    {
-                    Amount = 5,
-                    BillStatus = BillStatus.Paid,
-                    DateTimeCreated = DateTime.Now,
-                    DateTimeFinished = DateTime.Now,
-                    ID = 888,
-                    PaymentType = PaymentType.CreditCard
-                    }
-                },
+            var result = _billService.GetAll();
 
-                {
-                    new Bill()
-                    {
-                    Amount = 2,
-                    BillStatus = BillStatus.WaitingToPay,
-                    DateTimeCreated = DateTime.Now,
-                    DateTimeFinished = DateTime.Now,
-                    ID = 848,
-                    PaymentType = PaymentType.CreditCard
-                    }
-                },
+            if (result != null) return Ok(result);
 
-             };
+            return NoContent();
         }
+
+        [HttpGet("getById")]
+        public IActionResult Get(int id)
+        {
+            var result = _billService.GetById(id);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] Bill bill)
+        {
+            if (bill == null) return BadRequest();
+
+            var result = _billService.Create(bill);
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest(id);
+
+            var result = _billService.Delete(id);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update([FromBody] Bill bill)
+        {
+            if (bill == null) return BadRequest();
+
+            var searchBill = _billService.GetById(bill.ID);
+
+            if (searchBill == null) return NotFound(bill);
+
+            var result = _billService.Update(bill);
+
+            if (result != null) return Ok(result);
+
+            return NotFound(result);
+        }
+
+
+
     }
 }
