d8d0708b7974d6c38eb0824d17e166b5356c3fbb
b.serafimov <b.b.serafimov@gmail.com>
2021-12-20 16:34:28 +0200

    fix TagController

--- BarManagerA/Extensions/AutoMapping.cs
+++ BarManagerA/Extensions/AutoMapping.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
 using BarManagerA.Models.DTO;
 using BarManagerA.Models.Requests;
 using BarManagerA.Models.Responses;
@@ -10,8 +12,10 @@ namespace BarManagerA.Host.Extensions
         public AutoMapping()
         {
             CreateMap<Tag, TagResponse>()
-                .ForMember(m => m.SpecialName, tm => tm.MapFrom(x => x.Id + x.Name));
-            CreateMap<TagRequest, Tag>();
+                .ForMember(m => m.SpecialName, tm => tm.MapFrom(x => x.Id + x.Name))
+                .ReverseMap();
+            CreateMap<TagRequest, Tag>().ReverseMap();
+            //CreateMap<IEnumerable<Tag>, IEnumerable<TagResponse>>().ReverseMap();
 
             CreateMap<Employee, EmployeeResponse>()
                 .ForMember(m => m.Id, tm => tm.MapFrom(x => x.Id + x.Name));
