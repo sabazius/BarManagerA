bd89e6b4ca132b5a3ae1f100cde108249aff7537
DenitsaAng <denica0angelieva@gmail.com>
2022-01-07 17:02:07 +0200

    Create DL layer and ClientTable repository and controller

---
+++ BarManagerA.DL/InMemoryDB/ClientTableInMemoryCollection.cs
@@ -0,0 +1,27 @@
+﻿using BarManagerA.Models.DTO;
+using System.Collections.Generic;
+
+namespace BarManagerA.DL.InMemoryDB
+{
+    public class ClientTableInMemoryCollection
+    {
+        public static List<ClientTable> ClientTableDB = new List<ClientTable>()
+        {
+            new ClientTable()
+            {
+                ID =1,
+                Seats = 2
+            },
+            new ClientTable()
+            {
+                ID = 2,
+                Seats = 4
+            },
+            new ClientTable()
+            {
+                ID = 3,
+                Seats = 5
+            }
+        };
+    }
+}
