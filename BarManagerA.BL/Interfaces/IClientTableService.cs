5e9c1b33d6c5186c75b34913fcc5f2641a90460c
DenitsaAng <denica0angelieva@gmail.com>
2022-01-07 21:36:50 +0200

    Half done ClientTable Service

---
+++ BarManagerA.BL/Interfaces/IClientTableService.cs
@@ -0,0 +1,19 @@
+﻿using BarManagerA.Models.DTO;
+using System.Collections.Generic;
+
+namespace BarManagerA.BL.Interfaces
+{
+    public interface IClientTableService
+    {
+        ClientTable Create(ClientTable clienttable);
+
+        ClientTable Update(ClientTable clienttable);
+
+        ClientTable Delete(int id);
+
+        ClientTable GetById(int id);
+
+        IEnumerable<ClientTable> GetAll();
+    }
+}
+
