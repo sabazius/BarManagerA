4d8da5fdf47ed37673ea80069e547cf27086f72d
DenitsaAng <denica0angelieva@gmail.com>
2022-01-08 20:14:26 +0200

    ClientTable Request & Response

---
+++ BarManagerA.Models/Requests/ClientTableRequest.cs
@@ -0,0 +1,13 @@
+﻿using BarManagerA.Models.DTO;
+using System.Collections.Generic;
+
+namespace BarManagerA.Models.Requests
+{
+    class ClientTableRequest
+    {
+        public int Seats { get; set; }
+        public List<Location> Location { get; set; }
+        public List<int> Orders { get; set; }
+        public List<int> Furniture { get; set; }
+    }
+}
